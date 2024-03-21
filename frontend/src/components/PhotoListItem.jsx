a864961066141057a054cf2b3cf5dbf0f6b6104b
mariefranceperrier <mariefrance.perrier@gmail.com>
2024-03-21 17:02:06 -0400

    access individual props directly instead of passing entire sampleDataForPhotoListItem object

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -9,21 +9,21 @@ const PhotoListItem = (props) => {
     <div className="photo-list-item">
       <img
         className="photo-list-item__image"
-        src={props.sampleDataForPhotoListItem.imageSource}
+        src={props.imageSource}
         alt="A picture of Montreal"
       />
       <div className="photo-list-item__details">
         <img
           className="photo-list-item__profile"
-          src={props.sampleDataForPhotoListItem.profile}
+          src={props.profile}
           alt="Profile"
         />
         <div className="photo-list-item__username">
-          <span>{props.sampleDataForPhotoListItem.username}</span>
+          <span>{props.username}</span>
         </div>
         <div className="photo-list-item__location">
-          <span>{props.sampleDataForPhotoListItem.location.city}</span>
-          <span>{props.sampleDataForPhotoListItem.location.country}</span>
+          <span>{props.location.city}</span>
+          <span>{props.location.country}</span>
         </div>
       </div>
     </div>
