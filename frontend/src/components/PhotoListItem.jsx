e0c1b42e78e2629e285f11621239eab7d50daf2f
mariefranceperrier <mariefrance.perrier@gmail.com>
2024-03-21 17:40:36 -0400

    adjust className to apply BEM naming convention

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -6,25 +6,27 @@ import "../styles/PhotoListItem.scss";
 const PhotoListItem = (props) => {
   /* Insert React */
   return (
-    <div className="photo-list-item">
+    <div className="photo-list_item">
       <img
-        className="photo-list-item__image"
+        className="photo-list__image"
         src={props.imageSource}
         alt="A picture of Montreal"
       />
-      <div className="photo-list-item__details">
+      <div className="photo-list__details">
         <img
-          className="photo-list-item__profile"
+          className="photo-list__profile"
           src={props.profile}
           alt="Profile"
         />
-        <div className="photo-list-item__username">
-          <span>{props.username}</span>
-        </div>
-        <div className="photo-list-item__location">
-          <span>{props.location.city}</span>
-          <span>{props.location.country}</span>
-        </div>
+        <div className="photo-list__user-info">
+          <div className="photo-list__username">
+            <span>{props.username}</span>
+          </div>
+          <div className="photo-list__location">
+            <span>{props.location.city}</span>
+            <span>{props.location.country}</span>
+          </div>
+        </div> 
       </div>
     </div>
   );
