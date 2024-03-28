4614486fab2a53fc5d1803ac52e1776fdd64409e
mariefranceperrier <mariefrance.perrier@gmail.com>
2024-03-28 11:47:49 -0400

    implement useReducer

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,36 +1,59 @@
-import {useState} from 'react';
+import {useReducer} from 'react';
+
+// Define action types
+const SET_MODAL_DATA = 'SET_MODAL_DATA';
+const UPDATE_FAV_PHOTO_IDS = 'UPDATE_FAV_PHOTO_IDS';
+const CLOSE_MODAL = 'CLOSE_MODAL';
+
+// Define reducer function
+const reducer = (state, action) => {
+  switch (action.type) {
+    case SET_MODAL_DATA:
+      return { ...state, modalData: action.payload };
+    case UPDATE_FAV_PHOTO_IDS:
+      const { photoId } = action.payload;
+      const favoritePhotos = state.favoritePhotos.includes(photoId)
+        ? state.favoritePhotos.filter(id => id !== photoId)
+        : [...state.favoritePhotos, photoId];
+      return { ...state, favoritePhotos };
+    case CLOSE_MODAL:
+      return { ...state, modalData: null };
+    default:
+      return state;
+  }
+};
+
 
 const useApplicationData = () => {
-  const [modalData, setModalData] = useState(null);
-  const [favoritePhotos, setFavoritePhotos] = useState([]);
-
-  const updateToFavPhotoIds = (photoId) => { // Function to handle favoriting a photo, called handleFavorite in components.
-    if (favoritePhotos.includes(photoId)) {
-      setFavoritePhotos(prevPhotos => prevPhotos.filter(id => id !== photoId));
-    } else {
-      setFavoritePhotos(prevPhotos => [...prevPhotos, photoId]);
-    }
+  const initialState = {
+    modalData: null,
+    favoritePhotos: [],
   };
+// useReducer to manag state and dispatch actions
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  const setPhotoSelected = (photoData) => { // Function to set the selected photo in the modal, called handleModalDisplay in components.
-    setModalData(photoData);
-  };
+  // Action creators
+  const setPhotoSelected = (photoData) => {
+    dispatch({ type: SET_MODAL_DATA, payload: photoData });
+  }
 
-  const onClosePhotoDetailsModal = () => { // Function to close the modal, called handleCloseModal in components.
-    setModalData(null);
-  };
+  const updateToFavPhotoIds = (photoId) => {
+    dispatch({ type: UPDATE_FAV_PHOTO_IDS, payload: { photoId } });
+  }
+
+  const onClosePhotoDetailsModal = () => {
+    dispatch({ type: CLOSE_MODAL });
+  }
 
   return {
-    state: {
-      modalData,
-      favoritePhotos
-    },
+    state,
     actions: {
-      updateToFavPhotoIds,
       setPhotoSelected,
+      updateToFavPhotoIds,
       onClosePhotoDetailsModal
     }
   };
 };
+
 
 export default useApplicationData;
